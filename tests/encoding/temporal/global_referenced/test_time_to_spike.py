9e6ace49b9bdf07677e901e723751c212bf5ae12
TiloccaS <salvatore.tilocca@polito.it>
2025-06-12 11:03:19 +0200

    test: add multiple features to time to spike algorithm

--- tests/encoding/temporal/global_referenced/test_time_to_spike.py
+++ tests/encoding/temporal/global_referenced/test_time_to_spike.py
@@ -83,6 +83,20 @@ class TestTimeToFirstSpike(unittest.TestCase):
         result = time_to_first_spike(signal, interval)
         self.assertTrue(np.any(result))
 
+    def test_with_multiple_features(self):
+        """Test the function with a signal containing multiple features."""
+        np.random.seed(42)
+        signal = np.random.rand(10, 2)
+        interval = 2
+        encoded_signal = time_to_first_spike(signal, interval)
+        self.assertEqual(encoded_signal.shape, signal.shape)
+        signal_f1 = signal[:, 0]
+        signal_f2 = signal[:, 1]
+        encoded_signal_f1 = time_to_first_spike(signal_f1, interval)
+        encoded_signal_f2 = time_to_first_spike(signal_f2, interval)
+        np.testing.assert_array_equal(encoded_signal[:, 0], encoded_signal_f1)
+        np.testing.assert_array_equal(encoded_signal[:, 1], encoded_signal_f2)
+
 
 if __name__ == "__main__":
     unittest.main()
